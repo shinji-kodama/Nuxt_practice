cb64f048bcaa40c86c077fd1868e50b633ab4f41
keita_kawaguchi <keikawagu@gmail.com>
2021-08-16 22:18:54 +0900

    メイン画面のカードへステータスバーの追加

--- frontend/nuxt_app/pages/index.vue
+++ frontend/nuxt_app/pages/index.vue
@@ -20,9 +20,57 @@
       </div>
       <div class="w-4/5 bg-white m-4 rounded-3xl">
         <ul class="m-2 flex flex-wrap">
-          <li v-for="team in teams" :key="team.id" class="w-80 h-80 m-2 shadow">
-            <div class="h-2/4 shadow-sm"><img src="/football_fan.jpg" alt="" class="h-full w-full object-cover"></div>
-            <p class="p-2 overflow-hidden">{{ team.name }}</p>
+          <li v-for="team in teams" :key="team.id" class="w-80 h-4/6 m-2 shadow rounded-2xl">
+            <div class="h-2/4 shadow-sm"><img src="/football_fan.jpg" alt="" class="h-full w-full object-cover rounded-2xl"></div>
+            <p class="px-2 py-4 overflow-hidden">{{ team.name }}</p>
+            <div class="relative pt-1">
+            <div class="flex m-2 items-center justify-between">
+              <div>
+                <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full bg-blue-200">
+                  チームワーク
+                </span>
+              </div>
+              <div class="text-right">
+                <span class="text-xs font-semibold inline-block">
+                  30%
+                </span>
+              </div>
+            </div>
+            <div class="overflow-hidden h-2 mx-3 mb-4 text-xs flex rounded bg-blue-200">
+              <div style="width:30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
+            </div>
+            <div class="flex m-2 items-center justify-between">
+              <div>
+                <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full bg-red-200">
+                  技術力
+                </span>
+              </div>
+              <div class="text-right">
+                <span class="text-xs font-semibold inline-block">
+                  60%
+                </span>
+              </div>
+            </div>
+            <div class="overflow-hidden h-2 mx-3 mb-4 text-xs flex rounded bg-red-200">
+              <div style="width:60%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"></div>
+            </div>
+                        <div class="flex m-2 items-center justify-between">
+              <div>
+                <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full bg-green-200">
+                  戦術理解度
+                </span>
+              </div>
+              <div class="text-right">
+                <span class="text-xs font-semibold inline-block">
+                  80%
+                </span>
+              </div>
+            </div>
+            <div class="overflow-hidden h-2 mx-3 mb-4 text-xs flex rounded bg-green-200">
+              <div style="width:80%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"></div>
+            </div>
+          </div>
+          
             <!-- <button @click="remove(team.id)">X</button> -->
           </li>
         </ul>
