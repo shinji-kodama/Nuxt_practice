1dfd82572bb4b414e94180be27d98bd316b8710d
r0ckandc0de <yohtaro.s@gmail.com>
2021-08-31 18:35:45 +0900

    静的ファイル作成＋コンポーネント化

---
+++ frontend/nuxt_app/pages/menu/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <layout-wrapper>
+        <layout-visual
+            title="Menu"
+            :height="40"
+            visual="visual-menu"
+        />
+        <div class="container w-full mx-auto pt-20 pb-20 px-6 md:px-0">
+            <div class="mb-20">
+               <layout-menu-list />
+            </div>
+            <base-button name="トップへ戻る" link="/" />
+        </div>
+    </layout-wrapper>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'メニュー',
+    }
+  },
+}
+</script>
